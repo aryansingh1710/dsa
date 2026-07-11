a35d2d5a4547a4198ad580c0d82df27810e277a0
aryansingh1710 <aryansingh171019@gmail.com>
2026-07-11 13:54:38 +0530

    Time: 3 ms (21.57%) | Memory: 47.5 MB (15.39%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,32 +1,18 @@
 class Solution {
-    public int maxProduct(int[] nums) {
-        
-        int n = nums.length;
-        int leftProduct = 1;
-        int rightProduct = 1;
-        int ans = nums[0];
-        
-        for (int i = 0; i < n; i++) {
-            
-            // reset if product becomes zero
-            if (leftProduct == 0) {
-                leftProduct = 1;
-            }
-            
-            if (rightProduct == 0) {
-                rightProduct = 1;
-            }
-            
-            // prefix product (left to right)
-            leftProduct = leftProduct * nums[i];
-            
-            // suffix product (right to left)
-            rightProduct = rightProduct * nums[n - 1 - i];
-            
-            // update maximum answer
-            ans = Math.max(ans, Math.max(leftProduct, rightProduct));
+    public int maxProduct(int[] a) {
+        int n=a.length;
+        int i=0;
+        int minEnding=a[0];
+        int maxEnding=a[0];
+        int ans=a[0];
+        for (int j=1;j<n;j++){
+          int v1=a[j];
+          int v2=minEnding*a[j];
+          int v3=maxEnding*a[j];
+          minEnding=Math.min(Math.min(v2,v3),a[j]);
+          maxEnding=Math.max(Math.max(v2,v3),a[j]);
+          ans=Math.max(ans,Math.max(minEnding,maxEnding));
         }
-        
         return ans;
     }
 }
